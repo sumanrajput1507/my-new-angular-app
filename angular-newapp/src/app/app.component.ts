2b3e5400e260d3dc55bc754e54fa59b2b25dd161
sumanrajput1507 <suman.rajput1507@gmail.com>
2026-03-20 15:50:23 +0530

    main-page-component

--- angular-newapp/src/app/app.component.ts
+++ angular-newapp/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; 
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
